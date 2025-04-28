7c32dd777cbb0cdf04e6209a208291ee814eb26c
gabriel-saraiva-flooxai <gabriel.saraiva.flooxai@gmail.com>
2025-04-28 17:43:45 -0300

    feat: search TODOs and clear form

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -12,6 +12,8 @@ export default class Todo extends Component {
         super(props)
         this.state = { description: '', list: [] }
 
+        this.handleClear = this.handleClear.bind(this)
+        this.handleSearch = this.handleSearch.bind(this)
         this.handleMarkAsDone = this.handleMarkAsDone.bind(this)
         this.handleMarkAsPending = this.handleMarkAsPending.bind(this)
         this.handleRemove = this.handleRemove.bind(this)
@@ -20,9 +22,14 @@ export default class Todo extends Component {
         this.refresh()
     }
 
-    refresh() {
-        axios.get(`${URL}?sort=-createAt`)
-            .then(resp => this.setState({...this.state, description: '', list: resp.data}))
+    refresh(description = '') {
+        const search = description ? `&description__regex=/${description}/` : ''
+        axios.get(`${URL}?sort=-createAt${search}`)
+            .then(resp => this.setState({...this.state, description, list: resp.data}))
+    }
+
+    handleSearch() {
+        this.refresh(this.state.description)
     }
 
     handleChange(e) {
@@ -37,17 +44,21 @@ export default class Todo extends Component {
 
     handleRemove(todo) {
         axios.delete(`${URL}/${todo._id}`)
-            .then(resp => this.refresh())
+            .then(resp => this.refresh(this.state.description))
     }
 
     handleMarkAsDone(todo) {
         axios.put(`${URL}/${todo._id}`, { ...todo, done: true })
-            .then(resp => this.refresh())
+            .then(resp => this.refresh(this.state.description))
     }
 
     handleMarkAsPending(todo) {
         axios.put(`${URL}/${todo._id}`, { ...todo, done: false })
-            .then(resp => this.refresh())
+            .then(resp => this.refresh(this.state.description))
+    }
+
+    handleClear() {
+        this.refresh()
     }
 
     render() {
@@ -59,7 +70,9 @@ export default class Todo extends Component {
                 <TodoForm
                     description={this.state.description}
                     handleChange={this.handleChange}
-                    handleAdd={this.handleAdd} />
+                    handleAdd={this.handleAdd}
+                    handleSearch={this.handleSearch} 
+                    handleClear={this.handleClear} />
                 <TodoList
                     list={this.state.list}
                     handleMarkAsDone={this.handleMarkAsDone}
--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -18,6 +18,10 @@ export default function TodoForm(props) {
             <Grid cols='12 3 2'>
                 <IconButton style='primary' icon='plus'
                     onClick={props.handleAdd} />
+                <IconButton style='info' icon='search'
+                    onClick={props.handleSearch} />
+                <IconButton style='default' icon='close'
+                    onClick={props.handleClear} />
             </Grid>
         </div>
     );
